b658fbf2564d11ef3313075567f5dc528408dc65
Muhammad Harris <78906230+imharris24@users.noreply.github.com>
2022-11-22 15:24:16 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ string binaryCalculator::binaryAddition(string numberOne, string numberTwo) {
 	string result = "\0";
 	int i = 0, j = 0;
 	bool carry = 0;
-	for (i = numberOne.length() - 1, j = numberTwo.length() - 1; i > 0 || j > 0; i--, j--) {
+	for (i = numberOne.length() - 1, j = numberTwo.length() - 1; i >= 0 && j >= 0; i--, j--) {
 		if (numberOne[i] == '0' && numberTwo[j] == '0') {
 			if (carry) {
 				result = '1' + result;
@@ -61,17 +61,44 @@ string binaryCalculator::binaryAddition(string numberOne, string numberTwo) {
 			continue;
 		}
 	}
+	while (i >= 0 && carry) {
+		if (numberOne[i] == '0') {
+			carry = 0;
+			result = '1' + result;
+		}
+		else {
+			result = '0' + result;
+		}
+		i--;
+	}
+	while (i >= 0 && !carry) {
+		result = numberOne[i] + result;
+		i--;
+	}
+	while (j >= 0 && carry) {
+		if (numberTwo[j] == '0') {
+			carry = 0;
+			result = '1' + result;
+		}
+		else {
+			result = '0' + result;
+		}
+		j--;
+	}
+	while (j >= 0 && !carry) {
+		result = numberTwo[j] + result;
+		j--;
+	}
+	if (carry) {
+		result = '1' + result;
+		carry = 0;
+	}
 	return result;
 }
 
+
 int main() {
 	binaryCalculator calc;
-	cout << calc.binaryAddition("0", "0");
-	cout << calc.binaryAddition("1", "0");
-	cout << calc.binaryAddition("0", "1");
-	cout << calc.binaryAddition("1", "1");
-
-
 
 	return 0;
 }
